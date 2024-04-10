0d4f116bc64ec5cf3a181151a25634b7a5fc57a3
jualvareze <ju.alvareze@gmail.com>
2024-04-10 01:42:48 -0400

    añadir google analitics

--- src/views/SobreNosotros/SobreNosotros.jsx
+++ src/views/SobreNosotros/SobreNosotros.jsx
@@ -1,6 +1,8 @@
+import './sobre_nosotros.css'
 function SobreNosotros() {
     return (
         <>
+        <div className='sobre-nosotros'></div>
             <p>
                 ¡Descubre la frescura y la belleza de la naturaleza en nuestra tienda online!
                 En nuestro espacio verde, encontrarás una cuidadosa selección de plantas de alta calidad, cultivadas con amor y experiencia. Desde los clásicos hasta las últimas tendencias, tenemos algo para cada gusto y espacio.
